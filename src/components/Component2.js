6f0baec4dc0abce46cf55fac9b72bde39f1c1267
AsuKaur <asukaur99@gmail.com>
2021-12-28 13:39:09 +0530

    Created App and configured Tailwind

---
+++ src/components/Component2.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Component2() {
+  return <div>Hey! This is the second component.</div>;
+}
---
+++ src/components/Component3.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Component3() {
+  return <div>Hey! This is the Third Component.</div>;
+}
+
+export default Component3;
